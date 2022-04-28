6eb1d1b7abd1f5d08b7400ad8974ba450e8877f2
zjuadurey <1721966349@qq.com>
2022-04-29 00:26:26 +0800

    sh

--- plot/structure_factor_4*4.sh
+++ plot/structure_factor_4*4.sh
@@ -1,6 +1,6 @@
-for (( kx=0; kx <= 100; kx+=4 ))
+for (( kx=0; kx <= 100; kx+=5 ))
 do
-	for (( ky=0; ky <= 100; ky+=4 ))
+	for (( ky=0; ky <= 100; ky+=5 ))
 	do
     	python3 test_ffn_structure_factor_x.py 4 0.01 1000 $kx $ky
 		echo "sh: kx:$kx ky:$ky done"
--- plot/structure_factor_x_5*5.sh
+++ plot/structure_factor_x_5*5.sh
@@ -1,6 +1,6 @@
-for (( kx=0; kx <= 100; kx+=4 ))
+for (( kx=0; kx <= 100; kx+=5 ))
 do
-	for (( ky=0; ky <= 100; ky+=4 ))
+	for (( ky=0; ky <= 100; ky+=5 ))
 	do
 		echo "sh: kx:$kx ky:$ky start"
     	python3 test_ffn_structure_factor_x.py 5 0.01 1000 $kx $ky
