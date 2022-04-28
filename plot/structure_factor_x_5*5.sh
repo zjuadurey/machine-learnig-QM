3287ee97dba6779fbc3351af3c517561bc05798e
zjuadurey <1721966349@qq.com>
2022-04-29 00:11:00 +0800

    sh

--- plot/structure_factor_x_5*5.sh
+++ plot/structure_factor_x_5*5.sh
@@ -2,6 +2,6 @@ for (( ky=0; ky <= 100; ky+=4 ))
 do
 	for (( kx=0; kx <= 100; kx+=4 ))
 	do
-    	python3 test_ffn_structure_factor_xy.py 5 0.01 1000 $kx $ky
+    	python3 test_ffn_structure_factor_xx.py 5 0.01 1000 $kx $ky
 	done
 done
